160e274e56017bf733f3ff942e466212b64d8fbd
Josh <joshdavies11@aol.com>
2025-04-04 09:44:14 +0100

    Update rust-environment.sh

--- rust-environment.sh
+++ rust-environment.sh
@@ -4,7 +4,7 @@
 # settings take effect every time the server boots
 
 maxplayers=200
-servername="Voidbyte Survival+ | Modded Rust"
+servername="Voidbyte Survival+ | Modded Rust | Small Map"
 # uncomment this to enable EAC, for Linux clients this must be commented out
 ENABLE_RUST_EAC=1
 
@@ -17,13 +17,13 @@ ENABLE_RUST_EAC=1
 #     If you change this value, then a new map will be generated on next boot.
 #     The old map will still persist unless `./admin-actions/regenerate-map.sh`
 #     is called which deletes all maps
-seed=97845611
+seed=39
 
 # range: unknown, used to recover a known setting from an existing map.
 #salt=
 
 # default: 3000, range: 1000-6000, map size in meters.
-worldsize=5000
+worldsize=2000
 
 ####################
 # CUSTOM MAP SUPPORT
